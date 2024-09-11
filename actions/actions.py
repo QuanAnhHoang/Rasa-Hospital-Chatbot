ce551c5f9c55df3c665d07ded85fe809c8ff22a2
QuanAnhHoang <s3818278@rmit.edu.vn>
2024-09-11 23:28:03 +0700

    Enabled chat widget for website.
    updated credentials.yml so chat widget can be integrated to hospital website.

--- actions/actions.py
+++ actions/actions.py
@@ -5,22 +5,23 @@
 # https://rasa.com/docs/rasa/custom-actions
 
 
-from typing import Any, Text, Dict, List
-from rasa_sdk import Action, Tracker
-from rasa_sdk.executor import CollectingDispatcher
+# This is a simple example for a custom action which utters "Hello World!"
 
-class ActionGreetWithName(Action):
-    def name(self) -> Text:
-        return "action_greet_with_name"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-        
-        name = next(tracker.get_latest_entity_values("name"), None)
-        if name:
-            dispatcher.utter_message(template="utter_chao_hoi_voi_ten", name=name)
-        else:
-            dispatcher.utter_message(template="utter_chao_hoi_voi_ten", name="None")
-        
-        return []
+# from typing import Any, Text, Dict, List
+#
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
+#
+#
+# class ActionHelloWorld(Action):
+#
+#     def name(self) -> Text:
+#         return "action_hello_world"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+#         dispatcher.utter_message(text="Hello World!")
+#
+#         return []
